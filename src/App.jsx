7aa6a9b90fe0b1749d2f0559d03e7ad31404e015
Janko <mailodjanka@gmail.com>
2020-05-06 16:12:44 +0200

    Adds a button with an onClick event to switch names method

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,16 @@ class App extends Component {
       {name: 'Nikica', age: 29}
     ]
   }
+
+  switchNameHandler = () => {
+    console.log('stop this nonsense clicking')
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, there!</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Partying :)</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
