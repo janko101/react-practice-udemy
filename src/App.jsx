bf86bcbd64ce564813d03a6412efb7459d8f46ba
Janko <mailodjanka@gmail.com>
2020-05-06 16:18:56 +0200

    Adds a method to change component state with on click event

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,21 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
-    console.log('stop this nonsense clicking')
+  switchPersonsHandler = () => {
+    this.setState( {
+      persons: [
+        {name: 'Nikica', age: 29},
+        {name: 'Janko', age: 36},
+        {name: 'Helena', age: 28}
+      ]
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h1>Hi, there!</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchPersonsHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Partying :)</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
