127cf2d3a40cabbcf8c8bee4ff275cb4e615d607
Janko <mailodjanka@gmail.com>
2020-05-06 15:59:20 +0200

    Adds state properties to a component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import "./App.css";
 import Person from "./Components/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Janko', age: 36},
+      {name: 'Helena', age: 28},
+      {name: 'Nikica', age: 29}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, there!</h1>
-        <Person name="Janko" age="36" />
-        <Person name="Helena" age="28" />
-        <Person name="Nikica" age="29">My hobbies: Partying :)</Person>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Partying :)</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
