5312a099dc4eacd33dd7c170c7e3198890625295
Janko <mailodjanka@gmail.com>
2020-05-06 15:40:46 +0200

    Implements props in a Person component

--- src/Components/Person.jsx
+++ src/Components/Person.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Person = () => {
+const Person = (props) => {
   return (
     <p>
-      I'm a Person and I am {Math.floor(Math.random() * 30)} years old!
+      I'm {props.name} and I am {props.age} years old!
     </p>
   )
 }
