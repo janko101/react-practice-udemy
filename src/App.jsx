0207a5b76a196e642f4a9b6e202936908ad65044
Janko <mailodjanka@gmail.com>
2020-05-08 11:45:57 +0200

    Refactores change name handler method to change all the persons names with user input

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,22 @@ class App extends Component {
     showPersons: false,
   };
 
-  changeNameHandler = (event) => {
+  changeNameHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
+    })
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value
     
+    const persons = [...this.state.persons]
+    persons[personIndex] = person
+
     this.setState({
-      persons: [
-        { name: "Janko", age: 36 },
-        { name: event.target.value, age: 28 },
-        { name: "Nikica", age: 29 },
-      ],
+      persons: persons 
     });
   };
 
