f8880e696f0f3a4e480798df6102500bcc82bd81
Janko <mailodjanka@gmail.com>
2020-05-18 12:00:48 +0200

    Adds a high order component with class that has a classname forApp.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import classes from "./App.module.css";
 import PersonsList from "./Components/PersonsList";
 import Cockpit from "./Components/Cockpit";
+import WithClass from "./hoc/WithClass"
 
 class App extends Component {
   constructor(props) {
@@ -84,7 +85,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button
           onClick={() => {
             this.setState({ showCockpit: false });
@@ -101,7 +102,7 @@ class App extends Component {
           />
         ) : null}
         {persons}
-      </div>
+      </WithClass>
     );
   }
 }
