4b7a9ab0379ee056e43a86e1bb5e548f6837d427
Janko <mailodjanka@gmail.com>
2020-05-18 09:01:02 +0200

    Changes PersonsList component to class base component to be able to implement lifecycle hooks

--- src/Components/PersonsList.jsx
+++ src/Components/PersonsList.jsx
@@ -1,18 +1,20 @@
-import React from "react";
-import Person from "./Person"
+import React, { Component } from "react";
+import Person from "./Person";
 
-const PersonsList = (props) => {
-  console.log('[PersonsList], rendering...')
-  return props.persons.map((person, index) => {
-    return (
-      <Person
-        click={() => props.clicked(index)}
-        name={person.name}
-        age={person.age}
-        key={person.id}
-        changed={(event) => props.changed(event, person.id)}
-      />
-    );
-  });
+class PersonsList extends Component {
+  render() {
+    console.log("[PersonsList], rendering...");
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={(event) => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  }
 }
 export default PersonsList;
