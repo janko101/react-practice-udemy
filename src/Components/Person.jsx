b96584e9e743e515331862a4a242707d7f771a9b
Janko <mailodjanka@gmail.com>
2020-05-18 14:34:01 +0200

    Wraps a Person component in HOC WithClass to assign className

--- src/Components/Person.jsx
+++ src/Components/Person.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import classes from "./Person.module.css";
+import WithClass from "../hoc/WithClass"
 
 class Person extends Component {
   render() {
     console.log("[Person.jsx], rendering...");
     return (
-      <div className={classes.Person}>
+      <>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
@@ -15,9 +16,9 @@ class Person extends Component {
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+      </>
     );
   }
 }
 
-export default Person;
+export default WithClass(Person, classes.Person);
